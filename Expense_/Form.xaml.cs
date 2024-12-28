73f8f6d1807a0a6e054ec2c49c1f0c8d6dcb6b21
Ahmed mostafa <Ahmed mostafa@DESKTOP-BJQ2RLS>
2024-12-27 22:56:33 -0800

    Initial Commit

---
+++ Expense_/Form.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Expense_
+{
+
+    public partial class Form : Page
+    {
+
+        Person person = new Person()
+        {
+            FN = "Ahmed",
+            LN = "Mostafa",
+            Email = "ahmed22@gsm",
+            Address = "massrrrr",
+            Phone = 6
+
+        };
+        public Form()
+        {
+            InitializeComponent();
+
+        }
+
+        public class Person
+        {
+            public Person()
+            {
+
+            }
+            public string FN { get; set; }
+            public string LN {  get; set; }
+            public string Email { get; set; }
+            public int Phone { get; set; }
+            public string Address { get; set; }
+        }
+    }
+}
---
+++ Expense_/Reportxaml.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Expense_
+{
+    /// <summary>
+    /// Interaction logic for Reportxaml.xaml
+    /// </summary>
+    public partial class Reportxaml : Page
+    {
+        public Reportxaml()
+        {
+            InitializeComponent();
+        }
+        
+    }
+    public void ShowTheReport()
+    {
+        SqlConnection conn = new SqlConnection("Data Source = DESKTOP-45H0ONO\\SQLEXPRESS;Initial Catalog = WPF_Project;Inter");
+        conn.Open();
+
+        SqlCommand cmd = new SqlCommand("SELECT * FROM WPF_Project WHERE Names = @name", conn);
+        cmd.Parameters.AddWithValue("@name", report.FN);
+    }
+}
